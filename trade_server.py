edab2ae368c3591c0bc24cbbda789c0c919d439e
Michael The <mcgthe@gmail.com>
2015-05-11 13:43:36 +0200

    Server remembers messages sent to it.

--- trade_server.py
+++ trade_server.py
@@ -1,12 +1,16 @@
 import threading
 import SocketServer
 
+messages = []
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         while True:
             data = self.request.recv(1024)
+            if data:
+                messages.append(data)
+            print messages
             cur_thread = threading.current_thread()
             response = "{}: {}".format(cur_thread.name, data)
             self.request.sendall(response)
@@ -16,7 +20,7 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
 
-def create_server(host, port):
+def create_server(host="localhost", port=0):
     server = ThreadedTCPServer((host, port), ThreadedTCPRequestHandler)
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.daemon = True
