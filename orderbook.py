06e1e183730fb3c7f67b8eb1b4676f49368a4c3b
Michael The <mcgthe@gmail.com>
2015-05-12 13:18:03 +0200

    Add basic bid/offer matching.

--- orderbook.py
+++ orderbook.py
@@ -1,1 +1,12 @@
-__author__ = 'yuugo'
+
+bids = []
+offers = []
+
+
+def match_bid(bid):
+    offer = lowest_offer()
+    return offer if offer['price'] <= bid['price'] else None
+
+
+def lowest_offer(offers=offers):
+    return min(offers, key=lambda x: x['price'])
