94bbe444fb55da358d75352da7ce5b0ff0f49c8b
Michael The <mcgthe@gmail.com>
2015-05-18 14:26:06 +0200

    send_msg now returns response. Add handling for trade messages client-side.

--- trade_client.py
+++ trade_client.py
@@ -3,6 +3,9 @@ import socket
 import hashlib
 
 
+from orderbook import trade_offer, create_trade
+
+
 def send_msg(ip, port, message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
@@ -12,8 +15,25 @@ def send_msg(ip, port, message):
         print "Received: {}".format(response)
     finally:
         sock.close()
+    return response
 
 
 def send_offer(ip, port, offer):
     message = json.dumps(offer)
-    send_msg(ip, port, message)
+    return send_msg(ip, port, message)
+
+
+def handle_response(response):
+    response = json.loads(response)
+    if response['type'] == 'trade':
+        return handle_trade(response)
+    return "Nothing"
+
+def handle_trade(trade):
+    id = trade['trade-id'].split(';')[0]
+
+    return create_trade(
+        id = id,
+        quantity=trade['quantity'],
+        trade_id = trade['trade-id']
+    )
