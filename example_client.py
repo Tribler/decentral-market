cd8c73b7eef032e4d53e49e92a47b045be5cb8fb
Michael The <mcgthe@gmail.com>
2015-05-19 10:40:21 +0200

    Example now asks for ip address instead of just using localhost.

--- example_client.py
+++ example_client.py
@@ -3,12 +3,13 @@ from datetime import datetime
 from orderbook import create_msg
 from trade_client import send_offer, handle_response
 
+ip = raw_input("Enter ip address: ")
 port = raw_input("Enter port nr: ")
 for x in xrange(3):
     offer = create_msg(1, type="ask", price=3, quantity="4", timeout=datetime.now().isoformat())
 
     print "Offer: {}".format(str(offer))
-    response = send_offer('localhost', int(port), offer)
+    response = send_offer(ip, int(port), offer)
     print "Response: {}".format(str(response))
 
     print "\n"
@@ -16,5 +17,5 @@ for x in xrange(3):
     offer = handle_response(response)
     if offer:
         print "Offer: {}".format(str(offer))
-        response = send_offer('localhost', int(port), offer)
+        response = send_offer(ip, int(port), offer)
         print "Response: {}".format(str(response))
