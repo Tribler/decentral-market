5c4621990ad1d6790afac0307d1dc05328fe0b0b
Michael The <mcgthe@gmail.com>
2015-05-19 11:19:25 +0200

    Can now post bids using the example client.

--- example_client.py
+++ example_client.py
@@ -1,21 +1,35 @@
 from datetime import datetime
 
-from orderbook import create_msg
+from orderbook import create_ask, create_bid
 from trade_client import send_offer, handle_response
 
 ip = raw_input("Enter ip address: ")
 port = raw_input("Enter port nr: ")
-for x in xrange(3):
-    offer = create_msg(1, type="ask", price=3, quantity="4", timeout=datetime.now().isoformat())
 
-    print "Offer: {}".format(str(offer))
-    response = send_offer(ip, int(port), offer)
-    print "Response: {}".format(str(response))
+while True:
+    o = raw_input("Ask (1) or bid (2)?")
+    if o == "1":
+        offer = create_ask(1, price='3', quantity='6', timeout=datetime.now().isoformat())
 
-    print "\n"
-
-    offer = handle_response(response)
-    if offer:
         print "Offer: {}".format(str(offer))
         response = send_offer(ip, int(port), offer)
         print "Response: {}".format(str(response))
+
+        print "\n"
+
+        offer = handle_response(response)
+        if offer:
+            print "Offer: {}".format(str(offer))
+            response = send_offer(ip, int(port), offer)
+            print "Response: {}".format(str(response))
+    elif o == "2":
+        offer = create_bid(1, price='4', quantity='3', timeout=datetime.now().isoformat())
+        print "Offer: {}".format(str(offer))
+        response = send_offer(ip, int(port), offer)
+        print "Response: {}".format(str(response))
+
+        offer = handle_response(response)
+        if offer:
+            print "Offer: {}".format(str(offer))
+            response = send_offer(ip, int(port), offer)
+            print "Response: {}".format(str(response))
