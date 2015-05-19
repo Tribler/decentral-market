b6d96c90a942e6782bbd709e96416aa4b0ae271e
Michael The <mcgthe@gmail.com>
2015-05-19 13:02:26 +0200

    Update examples to reflect parameter changes: id is no longer required.

--- example.py
+++ example.py
@@ -44,9 +44,9 @@ server = create_server(host=host)
 print server.server_address
 
 # Create 3 bids with prices 6, 3, and 2.
-create_bid(1234, price=6, quantity=4, timeout=datetime.now().isoformat())
-create_bid(1234, price=3, quantity=4, timeout=datetime.now().isoformat())
-create_bid(1234, price=2, quantity=4, timeout=datetime.now().isoformat())
+create_bid(price=6, quantity=4, timeout=datetime.now().isoformat())
+create_bid(price=3, quantity=4, timeout=datetime.now().isoformat())
+create_bid(price=2, quantity=4, timeout=datetime.now().isoformat())
 
 print_all_offers()
 try:
--- example_client.py
+++ example_client.py
@@ -9,7 +9,7 @@ port = raw_input("Enter port nr: ")
 while True:
     o = raw_input("Ask (1) or bid (2)?")
     if o == "1":
-        offer = create_ask(1, price='3', quantity='6', timeout=datetime.now().isoformat())
+        offer = create_ask(price='3', quantity='6', timeout=datetime.now().isoformat())
 
         print "Offer: {}".format(str(offer))
         response = send_offer(ip, int(port), offer)
@@ -23,7 +23,7 @@ while True:
             response = send_offer(ip, int(port), offer)
             print "Response: {}".format(str(response))
     elif o == "2":
-        offer = create_bid(1, price='4', quantity='3', timeout=datetime.now().isoformat())
+        offer = create_bid(price='4', quantity='3', timeout=datetime.now().isoformat())
         print "Offer: {}".format(str(offer))
         response = send_offer(ip, int(port), offer)
         print "Response: {}".format(str(response))
