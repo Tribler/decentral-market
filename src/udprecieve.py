4108a4a79d082928f3d667593190d1df1eca0621
Michael The <mcgthe@gmail.com>
2015-05-22 15:56:46 +0200

    Move print functionality to utils module.

--- src/udprecieve.py
+++ src/udprecieve.py
@@ -6,49 +6,9 @@ from twisted.internet import reactor
 
 from crypto import get_public_bytestring
 from orderbook import (match_incoming_ask, match_incoming_bid,
-        get_bids, get_asks, get_own_bids, get_own_asks,
         trades, offers, get_offer, remove_offer,
         trade_offer, create_confirm, create_cancel)
-
-
-# Printing functions for testing
-def offer_to_string(offer):
-    s = "{\n"
-    for k, v in offer.iteritems():
-        if k == 'id':
-            v = v.split('\n')[1][:20] + '...'
-        s += "\t{}: {}\n".format(k, v)
-    s += "    }"
-    return s
-
-
-def offers_to_string(offers):
-    return '\n    '.join(offer_to_string(offer) for offer in offers)
-
-
-def print_all_offers():
-
-    print '''
-    Bids
-    =========
-    {}
-
-    Asks
-    ========
-    {}
-
-    Own bids
-    ========
-    {}
-
-    Own Asks
-    ========
-    {}
-
-    Trades
-    ========
-    {}
-    '''.format(*[offers_to_string(o) for o in (get_bids(), get_asks(), get_own_bids(), get_own_asks(), trades)])
+from utils import print_all_offers
 
 
 class UdpReceive(DatagramProtocol):
