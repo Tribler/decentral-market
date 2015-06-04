f5d7f1c58eba3419cab80ff0184f974d62ddfd75
Michael The <mcgthe@gmail.com>
2015-06-04 14:31:20 +0200

    Clear orderbook before tests to test a clean orderbook.

--- tests/test_orderbook.py
+++ tests/test_orderbook.py
@@ -1,6 +1,14 @@
+from nose import with_setup
+
 from tsukiji import orderbook as ob
 
 
+def clear_orderbook():
+    ob.offers = []
+    ob.trades = []
+
+
+@with_setup(clear_orderbook)
 def test_create_msg_incrementing_message_id():
     first_message = ob.create_msg()
     second_message = ob.create_msg()
@@ -9,11 +17,13 @@ def test_create_msg_incrementing_message_id():
     assert second_message['message-id'] == 1, 'Expected 1, got {}'.format(second_message['message-id'])
 
 
+@with_setup(clear_orderbook)
 def test_create_msg():
     message = ob.create_msg()
     assert type(message) == dict
 
 
+@with_setup(clear_orderbook)
 def test_create_msg_passing_options():
     options = {
         'hello': 'world',
@@ -24,6 +34,7 @@ def test_create_msg_passing_options():
     assert message['hello'] == 'world'
 
 
+@with_setup(clear_orderbook)
 def test_create_msg_passing_options_overriding_default():
     options = {
         'id': 1234,
@@ -34,10 +45,21 @@ def test_create_msg_passing_options_overriding_default():
     assert message['id'] == 1234
 
 
+@with_setup(clear_orderbook)
 def test_create_ask():
     import datetime
     ask = ob.create_ask(1, 1, datetime.datetime.now())
     assert ask['type'] == 'ask'
+    assert ask['price'] == 1
+    assert ask['quantity'] == 1
+    assert len(ob.offers) == 1
+
+
+@with_setup(clear_orderbook)
+def test_create_bid():
+    import datetime
+    ask = ob.create_bid(1, 1, datetime.datetime.now())
+    assert ask['type'] == 'bid'
     assert ask['price'] == 1
     assert ask['quantity'] == 1
     assert len(ob.offers) == 1
