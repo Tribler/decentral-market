3ad0346a570739778b400daf7f481e44ebd28887
Michael The <mcgthe@gmail.com>
2015-06-04 15:00:25 +0200

    Add tests for trade, confirm, cancel, greeting, and greeting_response generation.

--- tests/test_orderbook.py
+++ tests/test_orderbook.py
@@ -4,23 +4,24 @@ from tsukiji import orderbook as ob
 
 
 def clear_orderbook():
+    ob.message_id = 0
     ob.offers = []
     ob.trades = []
-
-
-@with_setup(clear_orderbook)
-def test_create_msg_incrementing_message_id():
-    first_message = ob.create_msg()
-    second_message = ob.create_msg()
-
-    assert first_message['message-id'] == 0, 'Expected 0, got {}'.format(first_message['message-id'])
-    assert second_message['message-id'] == 1, 'Expected 1, got {}'.format(second_message['message-id'])
 
 
 @with_setup(clear_orderbook)
 def test_create_msg():
     message = ob.create_msg()
     assert type(message) == dict
+
+
+@with_setup(clear_orderbook)
+def test_create_msg_incrementing_message_id():
+    m1 = ob.create_msg()
+    m2 = ob.create_msg()
+
+    assert m1['message-id'] == 0, 'Expected 0, got {}'.format(m1['message-id'])
+    assert m2['message-id'] == 1, 'Expected 1, got {}'.format(m2['message-id'])
 
 
 @with_setup(clear_orderbook)
@@ -63,3 +64,41 @@ def test_create_bid():
     assert ask['price'] == 1
     assert ask['quantity'] == 1
     assert len(ob.offers) == 1
+
+
+@with_setup(clear_orderbook)
+def test_create_trade():
+    trade = ob.create_trade('abcd', 1, 1)
+    assert trade['type'] == 'trade'
+    assert trade['recipient'] == 'abcd'
+    assert trade['quantity'] == 1
+    assert trade['trade-id'] == 1
+
+
+@with_setup(clear_orderbook)
+def test_create_confirm():
+    confirm = ob.create_confirm('abcd', 1)
+    assert confirm['type'] == 'confirm'
+    assert confirm['recipient'] == 'abcd'
+    assert confirm['trade-id'] == 1
+
+
+@with_setup(clear_orderbook)
+def test_create_cancel():
+    cancel = ob.create_cancel('abcd', 1)
+    assert cancel['type'] == 'cancel'
+    assert cancel['recipient'] == 'abcd'
+    assert cancel['trade-id'] == 1
+
+
+@with_setup(clear_orderbook)
+def test_create_greeting():
+    greeting = ob.create_greeting()
+    assert greeting['type'] == 'greeting'
+
+
+@with_setup(clear_orderbook)
+def test_create_greeting_response():
+    greeting_response = ob.create_greeting_response(['abcd', 'efgh'])
+    assert greeting_response['type'] == 'greeting_response'
+    assert greeting_response['peerlist'] == ['abcd', 'efgh']
