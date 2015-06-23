cda1164773fdaaa248fba2827588ac80089ecf49
Yuugoh <hugoreinbergen@hotmail.com>
2015-06-23 10:28:09 +0200

    Added payment options through paypal

---
+++ tsukiji/paypal.py
@@ -0,0 +1,23 @@
+import json
+import urllib2
+import webbrowser
+
+def paycall(email, amount):
+    heads = {'X-PAYPAL-SECURITY-USERID': 'hugoreinbergen-facilitator_api1.hotmail.com',
+     'X-PAYPAL-SECURITY-PASSWORD': '4SD6JYJ85P7HFTXP',
+     'X-PAYPAL-SECURITY-SIGNATURE': 'AiPC9BjkCyDFQXbSkoZcgqH3hpacAH1rfUmkHaF9hKeEjxkZAZwdieE2',
+     'X-PAYPAL-APPLICATION-ID': 'APP-80W284485P519543T',
+     'X-PAYPAL-REQUEST-DATA-FORMAT': 'JSON',
+     'X-PAYPAL-RESPONSE-DATA-FORMAT': 'JSON'}
+    payload = {'actionType': 'PAY',
+     'currencyCode': 'EUR',
+     'receiverList': {'receiver': [{'amount': amount,
+                                    'email': email}]},
+     'returnUrl': 'http://google.com',
+     'cancelUrl': 'http://bing.com',
+     'requestEnvelope': {'errorLanguage': 'en_US',
+                         'detailLevel': 'ReturnAll'}}
+    req = urllib2.Request('https://svcs.sandbox.paypal.com/AdaptivePayments/Pay/', json.dumps(payload), heads)
+    pay_key = json.loads(urllib2.urlopen(req).read())['payKey']
+    webbrowser.open_new_tab('https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_ap-payment&paykey=' + pay_key)
+
