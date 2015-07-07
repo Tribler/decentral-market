66bf3debc670cad88f24796f6c339f7d1827dfc3
Michael The <mcgthe@gmail.com>
2015-07-07 16:40:49 +0200

    Add a repl for controlling tsukiji.

--- tsukiji/trader.py
+++ tsukiji/trader.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8
 import json
+from twisted.protocols import basic
 from twisted.internet.protocol import DatagramProtocol
-from twisted.internet import reactor
+from twisted.internet import reactor, stdio
 
 from crypto import get_public_bytestring
 from orderbook import (match_incoming_ask, match_incoming_bid,
@@ -135,23 +137,78 @@ class Trader(DatagramProtocol):
             self.add_to_peerlist(key, value)
         return 'Peers added'
 
-    def send_demo_offer(self, ask=True):
-        if ask:
-            offer = create_ask(1, 1)
-        else:
-            offer = create_bid(1, 1)
+    def send_offer(self, offer):
         offer = json.dumps(offer)
         self.relay_message(offer)
 
+    def send_greeting(self):
+        greeting = json.dumps(greeting)
+        self.relay_message(greeting)
+
+
+class Repl(basic.LineReceiver):
+    '''Console input for controlling Tsukiji.'''
+    delimiter = '\n'
+
+    def connectionMade(self):
+        self.sendLine('tsukiji console. Type \'help\' for more info.')
+        self.trader = Trader('tsukiji')
+        reactor.listenMulticast(8005, self.trader, listenMultiple=True)
+
+    def lineReceived(self, line):
+        if not line: return
+
+        command_parts = line.split()
+        command = command_parts[0]
+        args = command_parts[1:]
+
+        try:
+            method = getattr(self, 'do_' + command)
+        except AttributeError, e:
+            self.sendLine('Error: no such command.')
+        else:
+            try:
+                method(*args)
+            except Exception, e:
+                self.sendLine('Error: ' + str(e))
+
+    def do_help(self, command=None):
+        """help [command]: List commands, or show help on the given command."""
+        if command:
+            self.sendLine(getattr(self, 'do_' + command).__doc__)
+        else:
+            commands = [cmd[3:] for cmd in dir(self) if cmd.startswith('do_')]
+            self.sendLine("Valid commands: " +" ".join(commands))
+
+
+    def do_ask(self, price=None, quantity=None):
+        '''ask [price, quantity]: Create and post an ask.'''
+        ask = create_ask(float(price), float(quantity))
+        self.trader.send_offer(ask)
+
+    def do_bid(self, price=None, quantity=None):
+        '''bid [price, quantity]: Create and post a bid.'''
+        bid = create_bid(float(price), float(quantity))
+        self.trader.send_offer(bid)
+
+    def do_greeting(self):
+        '''greeting: greet everybody in your peerlist.'''
+        print 'Saying hello to everybody.'
+        self.trader.send_greeting()
+
+    def do_offers(self):
+        '''offers: List all offers'''
+        print_all_offers()
+
+    def do_quit(self):
+        '''quit: Quit this session.'''
+        self.sendLine('さよなら')
+        self.transport.loseConnection()
+
+    def connectionLost(self, reason):
+        reactor.stop()
+
+
 if __name__ == '__main__':
-    trader = Trader("Fan of Tsukiji")
-    reactor.listenMulticast(8005, trader, listenMultiple=True)
-
-    choice = raw_input('1 or 2 ')
-    if choice == '1':
-        trader.send_demo_offer()
-    elif choice == '2':
-        trader.send_demo_offer(ask=False)
-
-    print_all_offers()
+    stdio.StandardIO(Repl())
     reactor.run()
